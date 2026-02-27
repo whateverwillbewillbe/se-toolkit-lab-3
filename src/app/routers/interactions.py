a28f9bc37abfde0b52648988efb37cb2e8d018e6
hahahi <verbovecz@list.ru>
2026-02-27 17:38:21 +0300

    fix: filter interactions by item_id instead of learner_id

--- src/app/routers/interactions.py
+++ src/app/routers/interactions.py
@@ -18,7 +18,5 @@ async def get_interactions(
     """Get all interactions, optionally filtered by item."""
     interactions = await read_interactions(session)
     if item_id is not None:
-        interactions = [
-            i for i in interactions if i.learner_id == item_id
-        ]  # BUG: should filter by i.item_id
+        interactions = [i for i in interactions if i.item_id == item_id]  # BUG: should filter by i.item_id
     return interactions
